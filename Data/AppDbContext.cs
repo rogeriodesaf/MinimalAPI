3bb1805e78abd1dffb6fb30e8be7cdde4fd2acbc
Rogério de Sá <rogeriodesaf@gmail.com>
2024-04-15 22:20:07 -0300

    Primeiro Commit da Aplicação

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApiProject.Models;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+ }
+      public DbSet<UsuarioModel> Usuarios { get; set; }
+}
---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimalApiProject.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+
+    }
+}
